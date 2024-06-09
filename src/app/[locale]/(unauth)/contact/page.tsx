382f532dd59a061106ba1900eedbe154cc951dd6
zapboy216 <zapboy216@gmail.com>
2024-06-09 08:28:15 -0400

    update en.json file contents

--- src/app/[locale]/(unauth)/contact/page.tsx
+++ src/app/[locale]/(unauth)/contact/page.tsx
@@ -1,11 +1,10 @@
-import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
 
 export async function generateMetadata(props: { params: { locale: string } }) {
   const t = await getTranslations({
     locale: props.params.locale,
-    namespace: 'Portfolio',
+    namespace: 'Contact',
   });
 
   return {
@@ -14,23 +13,11 @@ export async function generateMetadata(props: { params: { locale: string } }) {
   };
 }
 
-const Portfolio = () => {
-  const t = useTranslations('Portfolio');
+export default function About() {
+  const t = useTranslations('Contact');
 
   return (
     <>
-      <div className="grid grid-cols-1 justify-items-start gap-3 md:grid-cols-2 xl:grid-cols-3">
-        {Array.from(Array(6).keys()).map((elt) => (
-          <Link
-            className="hover:text-blue-700"
-            key={elt}
-            href={`/portfolio/${elt}`}
-          >
-            {t('portfolio_name', { name: elt })}
-          </Link>
-        ))}
-      </div>
-
       <h1>{t('heading')}</h1>
       <h2>{t('heading_two')}</h2>
       <p>{t('paragraph_one')}</p>
@@ -39,6 +26,4 @@ const Portfolio = () => {
       {t('cta')}
     </>
   );
-};
-
-export default Portfolio;
+}
